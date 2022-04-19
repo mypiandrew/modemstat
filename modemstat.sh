a9ea923654936e6e250d603b8ee303421cf51102
mypiandrew <andrew@embeddedpi.com>
2022-04-19 15:03:26 +0100

    Update modemstat.sh
    Moved printout to end

--- modemstat.sh
+++ modemstat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ############################################################################
-# ModemStat -- Version 2.0
+# ModemStat -- Version 2.0.1
 #
 # Runs a series of diagnostic tests on the modem to determine what the
 #
@@ -11,7 +11,7 @@
 # - Modem Type / IMEI / SIM Nunber/ Firmware version
 #
 # Copyright (c) 2016-2021 Andrew O'Connell and others
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -19,10 +19,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -30,28 +30,20 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# 
+#
 ############################################################################
 
-if [[ "$1" == "-q" ]] 
-then 
-	OUTPUT=1
-else	
-	OUTPUT=0
+if [[ "$1" == "-q" ]]
+then
+        OUTPUT=1
+else
+        OUTPUT=0
 fi
 
 
 #################### MODEM VENDOR CHECK
 
-if [[ ${OUTPUT} -eq 0 ]] 
-then 
-	echo -en "Modem Vendor                       : "
-else
-	echo -en "MODEMVENDOR="
-fi
-
 chat -Vs TIMEOUT 10 ECHO OFF "" "ATI5" "OK" >/dev/modemAT </dev/modemAT 2>/tmp/log
-
 REGEX_SIMCOM='.*SIMCOM.*'
 REGEX_QUECTEL='.*Quectel.*'
 REGEX_SIERRA='.*Sierra.*'
@@ -59,28 +51,21 @@ REGEX_SIERRA='.*Sierra.*'
 RESPONSE=$(</tmp/log)
 
 if [[ $RESPONSE =~ $REGEX_QUECTEL ]] ; then
-	MODEM=1   # QUECTEL
-	echo "QUECTEL"
+        MODEM=1   # QUECTEL
+        MODEMVENDOR="QUECTEL"
 elif [[ $RESPONSE =~ $REGEX_SIMCOM ]] ; then
-	MODEM=2   # SIMCOM/OTHER:
-	echo "SIMCOM"
+        MODEM=2   # SIMCOM/OTHER:
+        MODEMVENDOR="SIMCOM"
 elif [[ $RESPONSE =~ $REGEX_SIERRA ]] ; then
-	MODEM=3   # SIERRA WIRELESS
-	echo "SIERRA WIRELESS"
-else	
-	MODEM=0   # UNKNOWN
-	echo "UNKNOWN"
+        MODEM=3   # SIERRA WIRELESS
+        MODEMVENDOR="SIERRA-WIRELESS"
+else
+        MODEM=0   # UNKNOWN
+        MODEMVENDOR="UNKNOWN"
 fi
 
 #################### MODEM IMEI NUMBER
 
-if [[ ${OUTPUT} -eq 0 ]] 
-then 
-	echo -en "Modem IMEI Number                  : "
-else
-	echo -en "IMEI="
-fi
-	
 chat -Vs TIMEOUT 10 ECHO OFF "" "AT+CIMI" "OK" >/dev/modemAT </dev/modemAT 2>/tmp/log
 
 REGEX='^([0-9]+)$'
@@ -90,48 +75,33 @@ IMEI=""
 
 if [[ $RESPONSE =~ $REGEX ]]
 then
-		IMEI="${BASH_REMATCH[1]}"                        
+        IMEI="${BASH_REMATCH[1]}"
 fi
 
-echo $IMEI
 
 #################### SIM NUMBER
 
 # Doesn't work on Sierra Wireless Modems
 if [[ $MODEM != 3 ]]
 then
-	if [[ ${OUTPUT} -eq 0 ]] 
-	then 
-		echo -en "SIM ID Number                      : "
-	else
-		echo -en "SIM="
-	fi
+        chat -Vs TIMEOUT 10 ECHO OFF "" "AT+CCID" "OK" >/dev/modemAT </dev/modemAT 2>/tmp/log
 
+        REGEX='\+CCID: ([0-9]+)'
+        RESPONSE=$(</tmp/log)
 
-	chat -Vs TIMEOUT 10 ECHO OFF "" "AT+CCID" "OK" >/dev/modemAT </dev/modemAT 2>/tmp/log
+        SIMID=""
 
-	REGEX='\+CCID: ([0-9]+)'
-	RESPONSE=$(</tmp/log)
+        if [[ $RESPONSE =~ $REGEX ]]
+        then
+                SIMID="${BASH_REMATCH[1]}"
+        fi
 
-	SIMID=""
-
-	if [[ $RESPONSE =~ $REGEX ]]
-	then
-			SIMID="${BASH_REMATCH[1]}"                        
-	fi
-
-	echo $SIMID
+        #echo $SIMID
 fi
 
 #################### SIM &  PIN CHECK
 
 
-if [[ ${OUTPUT} -eq 0 ]] 
-then 
-echo -en "SIM Status                         : "	
-else
-	echo -en "SIMSTATUS="
-fi
 
 chat -Vs TIMEOUT 10 ECHO OFF "" "AT+CPIN?" "OK" >/dev/modemAT </dev/modemAT 2>/tmp/log
 
@@ -145,38 +115,24 @@ then
         CPIN="${BASH_REMATCH[1]}"
 fi
 
-if [[ ${OUTPUT} -eq 0 ]] 
-then 
-	if [[ $CPIN == "READY"   ]]; then
-		echo "SIM unlocked and ready";
-	elif [[ $CPIN == "SIM PIN" ]]; then
-		echo "SIM PIN LOCKED - MUST DEACTIVATE BEFORE USE WITH SYSTEM";
-	elif [[ $CPIN == "SIM PUK" ]]; then
-		echo "SIM LOCKED BY NETWORK OPERATOR - MUST DEACTIVATE BEFORE USE WITH SYSTEM";
-	elif [[ $CPIN == "BLOCKED" ]]; then
-		echo "LOCKED BY NETWORK OPERATOR - MUST DEACTIVATE BEFORE USE WITH SYSTEM\n";
-	elif [[ $CPIN == "PH-NET PIN" ]]; then
-		echo "MODEM LOCKED BY NETWORK OPERATOR - MUST DEACTIVATE BEFORE USE WITH SYSTEM";
-	else
-		echo " ** Infomation Not Available **"
-	fi
+if [[ $CPIN == "READY"   ]]; then
+        PINSTATUS="SIM unlocked and ready";
+elif [[ $CPIN == "SIM PIN" ]]; then
+        PINSTATUS="SIM PIN LOCKED - MUST DEACTIVATE BEFORE USE WITH SYSTEM";
+elif [[ $CPIN == "SIM PUK" ]]; then
+        PINSTATUS="SIM LOCKED BY NETWORK OPERATOR - MUST DEACTIVATE BEFORE USE WITH SYSTEM";
+elif [[ $CPIN == "BLOCKED" ]]; then
+        PINSTATUS="LOCKED BY NETWORK OPERATOR - MUST DEACTIVATE BEFORE USE WITH SYSTEM";
+elif [[ $CPIN == "PH-NET PIN" ]]; then
+        PINSTATUS="MODEM LOCKED BY NETWORK OPERATOR - MUST DEACTIVATE BEFORE USE WITH SYSTEM";
 else
-	echo "'${CPIN}'"
+        PINSTATUS=" ** Infomation Not Available **"
+        CPIN="NOTAVAILABLE"
 fi
-
-
-
-
-
 
 #################### SIGNAL QUALITY
 
-if [[ ${OUTPUT} -eq 0 ]] 
-then 
-	echo -en "Signal Quality                     : "
-else
-	echo -en "SIGNAL="
-fi
+
 
 chat -Vs TIMEOUT 10 ECHO OFF "" "AT+CSQ" "OK" >/dev/modemAT </dev/modemAT 2>/tmp/log
 
@@ -193,23 +149,9 @@ then
         CSQ2="${BASH_REMATCH[2]}"
 fi
 
-if [[ ${OUTPUT} -eq 0 ]] 
-then 
-	echo "${CSQ1}/32"
-else
-	echo $CSQ1
-fi
-
-
 
 #################### NETWORK REGISTRATION MODE
 
-if [[ ${OUTPUT} -eq 0 ]] 
-then 
-	echo -en "Network Registration Mode          : "
-else
-	echo -en "REGMODE="
-fi
 
 chat -Vs TIMEOUT 10 ECHO OFF "" "AT+COPS?" "OK" >/dev/modemAT </dev/modemAT 2>/tmp/log
 
@@ -230,69 +172,66 @@ then
 fi
 
 if [[ $COPS1 -eq 0 ]] ; then
-	if [[ ${OUTPUT} -eq 0 ]] ; then 	
-		echo  "Automatic network selection"
-	else
-		echo "AUTOMATIC"
-	fi
+        if [[ ${OUTPUT} -eq 0 ]] ; then
+                REGMODE="Automatic network selection"
+        else
+                REGMODE="AUTOMATIC"
+        fi
 elif [[ $COPS1 -eq 1 ]]; then
-	if [[ ${OUTPUT} -eq 0 ]] ; then 	
-		echo  "Manual network selection"
-	else	
-		echo "MANUAL"
-	fi
+        if [[ ${OUTPUT} -eq 0 ]]
+        then
+                REGMODE="Manual network selection"
+        else
+                REGMODE="MANUAL"
+        fi
 elif [[ $COPS1 -eq 2 ]]; then
-	if [[ ${OUTPUT} -eq 0 ]] ; then 	
-		echo "Deregister from network"
-	else	
-		echo "DE-REGISTERED"
-	fi
+        if [[ ${OUTPUT} -eq 0 ]]
+        then
+                REGMODE="Deregister from network"
+        else
+                REGMODE="DE-REGISTERED"
+        fi
 elif [[ $COPS1 -eq 3 ]]; then
-	if [[ ${OUTPUT} -eq 0 ]] ; then 	
-		echo "Set, (no registration/deregistration)"
-	else
-		echo "SET"
-	fi
+        if [[ ${OUTPUT} -eq 0 ]]
+        then
+                REGMODE="Set, (no registration/deregistration)"
+        else
+                REGMODE="SET"
+        fi
 elif [[ $COPS1 -eq 4 ]]; then
-	if [[ ${OUTPUT} -eq 0 ]] ; then 	
-		echo print "Manual selection with automatic fall back"
-	else
-		echo "MANUAL/AUTO"
-	fi
+        if [[ ${OUTPUT} -eq 0 ]]
+        then
+                REGMODE="Manual selection with automatic fall back"
+        else
+                REGMODE="MANUAL/AUTO"
+        fi
 fi
 
 
-if [ ! -z "$COPS1" ] 
+if [ ! -z "$COPS1" ]
 then
-
-	if  (( $COPS1 >= 0 )) || (( $COPS1 <= 4 ))
-	then
-			if [[ ${OUTPUT} -eq 0 ]] 
-			then 
-				echo "Network ID                         : ${COPS3}"
-			else
-				echo "NETWORKID='${COPS3}'"
-			fi
-	else
-			if [[ ${OUTPUT} -eq 0 ]] 
-			then 
-				echo "Network ID                         : ** Information Not Available"
-			else
-				echo "NETWORKID=NOTAVAILABLE"
-			fi
-	fi
+        if  (( $COPS1 >= 0 )) || (( $COPS1 <= 4 ))
+        then
+                if [[ ${OUTPUT} -eq 0 ]]
+                then
+                        NETWORKID="${COPS3}"
+                else
+                        NETWORKID="'${COPS3}'"
+		fi
+        fi
+else
+        if [[ ${OUTPUT} -eq 0 ]]
+        then
+                NETWORKID="** Information Not Available **"
+        else
+                NETWORKID="NOTAVAILABLE"
+        fi
 fi
 
 
 
 #################### NETWORK REGISTRATION STATE
 
-if [[ ${OUTPUT} -eq 0 ]] 
-then 
-	echo -en "Registration state                 : ";
-else
-	echo -en "REGSTATE="
-fi
 
 chat -Vs TIMEOUT 10 ECHO OFF "" "AT+CREG?" "OK" >/dev/modemAT </dev/modemAT 2>/tmp/log
 
@@ -306,48 +245,47 @@ if [[ $RESPONSE =~ $REGEX ]]
 then
         CREG1="${BASH_REMATCH[1]}"
         CREG2="${BASH_REMATCH[2]}"
+
+        if [[ $CREG2 -eq 0 ]] ; then
+                if [[ ${OUTPUT} -eq 0 ]] ; then
+                        REGSTATE="Not Registered, Not searching"
+                else
+                        REGSTATE="NOT-REGISTERED"
+                fi
+        elif [[ $CREG2 -eq 1 ]]; then
+                if [[ ${OUTPUT} -eq 0 ]] ; then
+                        REGSTATE="Registered to home network"
+                else
+                        REGSTATE="REGISTERED-HOME"
+                fi
+
+        elif [[ $CREG2 -eq 2 ]]; then
+                if [[ ${OUTPUT} -eq 0 ]] ; then
+                        REGSTATE="Not registered, searching for network"
+                else
+                        REGSTATE="SEARCHING"
+                fi
+        elif [[ $CREG2 -eq 3 ]]; then
+                if [[ ${OUTPUT} -eq 0 ]] ; then
+                        REGSTATE="Registration denied"
+                else
+                        REGSTATE="DENIED"
+                fi
+
+        elif [[ $CREG2 -eq 4 ]]; then
+                if [[ ${OUTPUT} -eq 0 ]] ; then
+                        REGSTATE="Registered, roaming"
+                else
+                        REGSTATE="REGISTERED-ROAMING"
+                fi
+        else
+                if [[ ${OUTPUT} -eq 0 ]] ; then
+                        REGSTATE=" ** Infomation Not Available **"
+                else
+                        REGSTATE="UNKNOWN"
+                fi
+        fi
 fi
-
-if [[ $CREG2 -eq 0 ]] ; then
-	if [[ ${OUTPUT} -eq 0 ]] ; then 	
-        echo "Not Registered, Not searching"
-	else
-		echo "NOT-REGISTERED"
-	fi
-elif [[ $CREG2 -eq 1 ]]; then
-	if [[ ${OUTPUT} -eq 0 ]] ; then 	
-        echo "Registered to home network"
-	else
-		echo "REGISTERED-HOME"
-	fi
-		
-elif [[ $CREG2 -eq 2 ]]; then
-	if [[ ${OUTPUT} -eq 0 ]] ; then 	
-        echo "Not registered, searching for network"
-	else
-		echo "SEARCHING"
-	fi
-elif [[ $CREG2 -eq 3 ]]; then
-	if [[ ${OUTPUT} -eq 0 ]] ; then 	
-        echo "Registration denied"      
-	else
-		echo "DENIED"
-	fi
-
-elif [[ $CREG2 -eq 4 ]]; then
-	if [[ ${OUTPUT} -eq 0 ]] ; then 	
-		echo "Registered, roaming"
-	else
-		echo "REGISTERED-ROAMING"
-	fi              
-else
-	if [[ ${OUTPUT} -eq 0 ]] ; then 	
-        echo " ** Infomation Not Available **"
-	else
-		echo "UNKNOWN"
-	fi              
-fi
-
 
 ###################### MODEM SPECIFIC PARTS ########################
 
@@ -366,20 +304,13 @@ if [[ $MODEM -eq 1 ]] ; then
         MODE1=""
         MODE2=""
         MODE3=""
+	
         if [[ $RESPONSE =~ $REGEX ]]
         then
-                        MODE1="${BASH_REMATCH[1]}"
-                        MODE2="${BASH_REMATCH[2]}"
-                        MODE3="${BASH_REMATCH[3]}"
+                MODE1="${BASH_REMATCH[1]}"
+                MODE2="${BASH_REMATCH[2]}"
+                MODE3="${BASH_REMATCH[3]}"
         fi
-
-		if [[ ${OUTPUT} -eq 0 ]] ; then 	
-			echo -e "Modem Operating Mode               : $MODE1"
-			echo -e "Modem Operating Band               : $MODE3"
-		else
-			echo "MODEMMODE='${MODE1}'"
-			echo "MODEMBAND='${MODE3}'"
-		fi
 
 ######## SIMCOM
 elif [[ $MODEM -eq 2 ]]; then
@@ -392,22 +323,18 @@ elif [[ $MODEM -eq 2 ]]; then
 
         MODE1=""
         MODE2=""
-		MODE3=""
+        MODE3=""
+	
         if [[ $RESPONSE =~ $REGEX ]]
         then
-                        MODE1="${BASH_REMATCH[1]}"
-                        MODE2="${BASH_REMATCH[2]}"
+                MODE1="${BASH_REMATCH[1]}"
+                MODE2="${BASH_REMATCH[2]}"
         fi
-		if [[ ${OUTPUT} -eq 0 ]] ; then 
-			echo -en "Modem Operating Mode               : "
-		else
-			echo -en "MODEMMODE="
-		fi
-		
-        if [[ $MODE2 -eq 0 ]]; then			
+
+        if [[ $MODE2 -eq 0 ]]; then
                 MODE3="No Service"
         elif [[ $MODE2 -eq 1 ]]; then
-				MODE3="GSM"
+                MODE3="GSM"
         elif [[ $MODE2 -eq 2 ]]; then
                 MODE3="GPRS"
         elif [[ $MODE2 -eq 3 ]]; then
@@ -446,8 +373,6 @@ elif [[ $MODEM -eq 2 ]]; then
                 MODE3="NOT-AVAILABLE"
         fi
 
-		echo $MODE3
-		
 ######## SIERRA WIRELESS
 elif [[ $MODEM -eq 3 ]] ; then
 
@@ -456,27 +381,60 @@ elif [[ $MODEM -eq 3 ]] ; then
         REGEX='\*CNTI: ([0-9]+),([a-zA-Z]+)'
         RESPONSE=$(</tmp/log)
 
-        MODE1=""        
+        MODE1=""
+
         if [[ $RESPONSE =~ $REGEX ]]
         then
-				MODE1="${BASH_REMATCH[2]}"
+                MODE1="${BASH_REMATCH[2]}"
         fi
-
-		if [[ ${OUTPUT} -eq 0 ]]; then 
-			echo -e "Modem Operating Mode               : $MODE1"
-		else
-			echo -e "MODEMMODE=$MODE1"
-		fi
-
 fi
 
 
+if [[ ${OUTPUT} -eq 0 ]]
+then
+        echo
+        echo -e "Modem Vendor                       : $MODEMVENDOR"
+        echo -e "Modem IMEI Number                  : $IMEI"
+        echo -e "SIM ID Number                      : $SIMID"
+        echo -e "SIM Status                         : $PINSTATUS"
+        echo -e "Signal Quality                     : ${CSQ1}/32"
+        echo -e "Network Registration Mode          : ${REGMODE}"
+        echo -e "Network ID                         : ${NETWORKID}"
+        echo -e "Registration state                 : ${REGSTATE}"
+        if [[ $MODEM -eq 1 ]] ; then
+                echo -e "Modem Operating Mode               : $MODE1"
+                echo -e "Modem Operating Band               : $MODE3"
+        elif [[ $MODEM -eq 2 ]] ; then
+                echo -e "Modem Operating Band               : $MODE3"
+        elif [[ $MODEM -eq 3 ]] ; then
+                echo -e "Modem Operating Mode               : $MODE1"
+        fi
+else
+        echo -e "MODEMVENDOR=$MODEMVENDOR"
+        echo -e "IMEI=$IMEI"
+        echo -e "SIM=$SIMID"
+        echo -e "SIMSTATUS=$CPIN"
+        echo -e "SIGNAL=$CSQ1"
+        echo -e "REGMODE=$REGMODE"
+        echo -e "NETWORKID=$NETWORKID"
+        echo -e "REGSTATE=$REGSTATE"
+        if [[ $MODEM -eq 1 ]] ; then
+                echo -e "MODEMMODE='${MODE1}'"
+                echo -e "MODEMBAND='${MODE3}'"
+        elif [[ $MODEM -eq 2 ]] ; then
+                echo -e "MODEMBAND='${MODE3}'"
+        elif [[ $MODEM -eq 3 ]] ; then
+                echo -e "MODEMMODE='${MODE1}'"
+        fi
+
+fi
+
 ##################### Display Modem Model Info
 if [[ ${OUTPUT} -eq 0 ]]
-then 
-	chat -Vs TIMEOUT 10 ECHO OFF "" "ATI5" "OK" >/dev/modemAT </dev/modemAT 2>/tmp/log
+then
+        chat -Vs TIMEOUT 10 ECHO OFF "" "ATI5" "OK" >/dev/modemAT </dev/modemAT 2>/tmp/log
 
-	echo -e "Modem Specification   : \n"
-	cat /tmp/log | head -n -1 | tail -n +2 | grep -v '^[[:space:]]*$'
-fi 
- 
+        echo -e "Modem Specification   : \n"
+        cat /tmp/log | head -n -1 | tail -n +2 | grep -v '^[[:space:]]*$'
+	echo
+fi
